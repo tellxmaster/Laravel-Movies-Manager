da20efc4a3afc04946cbd72a3d63f33f4d1b5fcb
tellxmaster <telleriaelias@gmail.com>
2022-07-03 20:53:45 -0500

    Arregladisimo

--- resources/views/livewire/reporte-socio/view.blade.php
+++ resources/views/livewire/reporte-socio/view.blade.php
@@ -44,30 +44,69 @@
    </div>
 <div class="row">
    @if($spm)
-   <table class="table table-bordered">
-      <thead>
-         <tr class="bg-danger">
-            <th scope="col" class="text-center">Mes</th>
-            <th scope="col" class="text-center">Numero Socios</th>
-            <th scope="col" class="text-center">Nombre socio top</th>
-         </tr>
-      </thead>
-     <tbody>
-     @foreach($meses as $num=>$nombre)
-     <tr>
-        <td class="text-center">{{$nombre}}</td>
-        <td>{{$spm[$loop->iteration-1]}}</td>
-        <td>{{$apm[$loop->iteration-1]}}</td>
-     </tr>
-     @endforeach
-     </tbody>
-   </table>
-   <script>
-      let valores = [];
+   <div class="col-6">
+      <table class="table table-bordered">
+         <thead>
+            <tr class="bg-danger">
+               <th scope="col" class="text-center">Mes</th>
+               <th scope="col" class="text-center">Numero Socios</th>
+               <th scope="col" class="text-center">Nombre socio top</th>
+            </tr>
+         </thead>
+      <tbody>
       @foreach($meses as $num=>$nombre)
-        valores.push({{$spm[$loop->iteration-1]}});
+      <tr>
+         <td class="text-center">{{$nombre}}</td>
+         <td>{{$spm[$loop->iteration-1]}}</td>
+         <td>{{$apm[$loop->iteration-1]}}</td>
+      </tr>
       @endforeach
-   </script>
+      </tbody>
+      </table>
+   </div>
+   <div class="col-6">
+      <canvas id="myChart" style="min-height: 250px; height: 350px; max-height: 350px; max-width: 100%; min-width: 100%;"></canvas>
+   </div>
+   <script wire:poll.750ms>
+      let valores = {{Js::from($spm)}};
+      console.log(valores[0]);
+      const ctx = document.getElementById('myChart').getContext('2d');
+      const myChart = new Chart(ctx, {
+         type: 'line',
+         data: {
+            labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+            datasets: [{
+                  label: 'Socios',
+                  data: valores,
+                  backgroundColor: [
+                     'rgba(255, 99, 132, 0.2)',
+                     'rgba(54, 162, 235, 0.2)',
+                     'rgba(255, 206, 86, 0.2)',
+                     'rgba(75, 192, 192, 0.2)',
+                     'rgba(153, 102, 255, 0.2)',
+                     'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                     'rgba(255, 99, 132, 1)',
+                     'rgba(54, 162, 235, 1)',
+                     'rgba(255, 206, 86, 1)',
+                     'rgba(75, 192, 192, 1)',
+                     'rgba(153, 102, 255, 1)',
+                     'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+            }]
+         },
+         options: {
+            maintainAspectRatio: false,
+            scales: {
+                  y: {
+                     beginAtZero: true
+                  }
+            }
+         }
+      });
+      </script>
    @endif
 </div>
 
